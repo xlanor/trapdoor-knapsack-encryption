bdb3aa3b75d476c08de4accbc4456a331d4e77c4
xlanor <contact@jingk.ai>
2019-11-06 17:30:57 +0800

    Adding styling to new home page

--- components/HomePage/styles/homePageParent.js
+++ components/HomePage/styles/homePageParent.js
@@ -1,8 +1,47 @@
+import { COLORS } from '../../../constants/Colors';
+
+const imageStyleArrow = {
+  alignSelf: 'center',
+  width: 150,
+  height: 150,
+};
+
 const imageStyle = {
-  width: 50,
-  height: 50,
+  alignSelf: 'center',
+  width: 70,
+  height: 70,
+};
+
+const iconsView = {
+  flex:1,
+  marginTop:100,
+  marginBottom: 20,
 }
 
+
+const textBox = {
+  backgroundColor: COLORS.RED_1,
+  alignSelf: 'center',
+  fontFamily: 'open-sans',
+  fontSize: 40,
+  color: COLORS.YELLOW_1,
+  minWidth: 350,
+  textAlign: 'center',
+  borderRadius: 10,
+  borderColor: COLORS.RED_1,
+  borderWidth: 1,
+  overflow: 'hidden',
+  padding:10,
+}
+
+const textWrapperStyle = { 
+    flex:1, 
+    alignItems: 'center', 
+}
 export default {
   imageStyle,
+  textBox,
+  textWrapperStyle,
+  imageStyleArrow,
+  iconsView
 }
--- constants/Colors.js
+++ constants/Colors.js
@@ -2,5 +2,7 @@ const tintColor = '#2f95dc';
 
 export const COLORS = {
   ORANGE_1: '#FFA200',
+  RED_1: '#FF3D3D',
+  YELLOW_1: '#FEF6A7',
 };
 
