1b7e91cd288d9a826920856e3d186e17c248ded7
xlanor <contact@jingk.ai>
2019-12-02 23:54:03 +0800

    Fixed android overlapping on screen 2

--- screens/styles/learnScreen.js
+++ screens/styles/learnScreen.js
@@ -1,7 +1,8 @@
 import { COLORS } from '../../constants/Colors';
+import { Platform, StatusBar} from 'react-native';
 
 const backGroundContainer = {
-
+  paddingTop: Platform.OS === 'ios' ? StatusBar.currentHeight : Platform.OS === 'android'? StatusBar.currentHeight: 0,
   flex: 1,
   backgroundColor: COLORS.ORANGE_1,
 };
