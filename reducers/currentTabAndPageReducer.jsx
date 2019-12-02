1b1e39dcebd968aab907764c67c0717c572b1623
xlanor <contact@jingk.ai>
2019-12-02 23:36:11 +0800

    Added state transitions for intro - jump to next page

--- reducers/currentTabAndPageReducer.jsx
+++ reducers/currentTabAndPageReducer.jsx
@@ -11,24 +11,21 @@ const initialState = {
   tabName: "intro",
   tabPage: 1,
   minTabPage: 0,
-  maxIntroPages: 5,
+  maxIntroPages: 2,
 }
 
-const nextPage = (tabName) =>{
-
-} 
 
 const currentTabAndPageReducer = (state=initialState, action) =>{
   switch(action.type){
       case NEXT_INTRO_PAGE:
           return {
             ...state,
-            tabPage: (tabPage+1 > maxIntroPages) ?  tabPage: tabPage+1,
+            tabPage: (state.tabPage+1 > state.maxIntroPages) ?  state.tabPage: state.tabPage+1,
           }
       case PREVIOUS_INTRO_PAGE:
           return {
             ...state,
-            tabPage: (tabPage-1 < 0) ? 0 : tabPage-1,
+            tabPage: (state.tabPage-1 < 0) ? 0 : state.tabPage-1,
           }
       case RESET_PAGE:
           return initialState;
