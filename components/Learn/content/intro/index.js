156ae9513f6bc7723507767de527a5f3cf37a7cf
xlanor <contact@jingk.ai>
2019-12-03 09:36:36 +0800

    Added pages 3 and 4 for intro

--- components/Learn/content/intro/index.js
+++ components/Learn/content/intro/index.js
@@ -1,6 +1,11 @@
 import pageOne from './page1';
 import pageTwo from './page2';
+import pageThree from './page3';
+import pageFour from './page4';
 
-export { pageOne as introPageOne,
-  pageTwo as introPageTwo 
+export { 
+  pageOne as introPageOne,
+  pageTwo as introPageTwo,
+  pageThree as introPageThree,
+  pageFour as introPageFour,
 };
---
+++ components/Learn/content/intro/page3.js
@@ -0,0 +1,7 @@
+export default pageThree = {
+    "title":"<h1>Brief History of Trapdoor Knapsack</h1>",
+    "text": `<p>The trapdoor knapsack is also known as the Merkle-Hellman knapsack cryptosystem and is invented by Ralph Merkle and Martin Hellman in 1978. The general idea behind this algorithm is to create two separate problems - one of which is easy to solve, giving the private key, and the other being difficult to solve, giving a public key. This ensures that the private key is easy to use, but the public key is difficult to compute. As such, this functions as a “trapdoor”, ensuring that without proper knowledge of the trapdoor, the encryption cannot easily be reversed. However Trapdoor knapsack has been broken.
+    
+  </p>`
+    
+  }
