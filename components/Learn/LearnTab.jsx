49825bc32831dbdf2d9378896e2a85fc70025a32
xlanor <contact@jingk.ai>
2019-12-22 13:20:13 +0800

    Added TouchableWithoutFeedBack with keyboard.dismiss

--- components/Learn/LearnTab.jsx
+++ components/Learn/LearnTab.jsx
@@ -325,43 +325,44 @@ class LearnTab extends Component{
     render(){
       return(
         <View style={{flex:1}}>
-            <SafeAreaView style={styles.learnTab.scrollViewWrapper}>
+            <View style={styles.learnTab.scrollViewWrapper}>
 
                 {this.loadPage()}
-            </SafeAreaView>
-
-            <View style={styles.learnTab.bottom}>
-                <View style={styles.learnTab.borderWrapper}>
-                  <View style={styles.learnTab.leftArrowWrapper}>
-                    {
-                      this.isFirstPage()?
-                      null:
-                      <TouchableOpacity onPress = {()=>{
-                          this.getTouchablePreviousAction()
-                      }}>
-                      <Image style={styles.learnTab.nextArrowSize} source={ BackArrow } />
-                      </TouchableOpacity>
-                    }
-                  </View>
-                  <View style={styles.learnTab.centerFooterWrapper}>
-                    {/* To put the center footer here later. */}
-                  </View>
-                  <View style={styles.learnTab.rightArrowWrapper}>
+           
+              <View style={styles.learnTab.bottom}>
+                  <View style={styles.learnTab.borderWrapper}>
+                    <View style={styles.learnTab.leftArrowWrapper}>
                       {
-                        this.isFinalPage()?
-                        this.getNextTab():
-                          this.canNavigate()?
-                            <TouchableOpacity onPress = {()=>{
-                                this.getTouchableNextAction()
-                            }}>
-                              
-                              <Image style={styles.learnTab.nextArrowSize} source={ FrontArrow} />
-                            </TouchableOpacity>
-                            : null
+                        this.isFirstPage()?
+                        null:
+                        <TouchableOpacity onPress = {()=>{
+                            this.getTouchablePreviousAction()
+                        }}>
+                        <Image style={styles.learnTab.nextArrowSize} source={ BackArrow } />
+                        </TouchableOpacity>
                       }
+                    </View>
+                    <View style={styles.learnTab.centerFooterWrapper}>
+                      {/* To put the center footer here later. */}
+                    </View>
+                    <View style={styles.learnTab.rightArrowWrapper}>
+                        {
+                          this.isFinalPage()?
+                          this.getNextTab():
+                            this.canNavigate()?
+                              <TouchableOpacity onPress = {()=>{
+                                  this.getTouchableNextAction()
+                              }}>
+                                
+                                <Image style={styles.learnTab.nextArrowSize} source={ FrontArrow} />
+                              </TouchableOpacity>
+                              : null
+                        }
+                    </View>
                   </View>
-                </View>
-          </View>
+              </View>
+            </View>
+
         </View>
       );
     }
