2b492f02640ea66e4915cc0d2fd5a091e24d9335
xlanor <contact@jingk.ai>
2019-12-24 18:01:49 +0800

    Restyled alertpopup for accessing tab that was not previously granted access yet

--- components/Common/AlertPopUp.jsx
+++ components/Common/AlertPopUp.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { Modal, TouchableOpacity, TouchableWithoutFeedback, View, Text } from 'react-native'
+import {
+   Modal, 
+   TouchableOpacity, 
+   TouchableWithoutFeedback, 
+   View, 
+   Text, 
+   Image
+} from 'react-native'
 import PropTypes from 'prop-types'
 import { alertPopUp as styles } from './styles';
 
 const AlertPopUp = (props) => {
-  const { callback, messageContent, visibility } = props;
+  const { callback, messageContent, visibility, icon } = props;
   return (
     <Modal animationType="fade" transparent visible={visibility}>
       <TouchableOpacity style={styles.modalBackground}  onPress={() => callback()}>
         <TouchableWithoutFeedback>
           <View style={styles.modalContainer}>
-            <Text>{messageContent}</Text>
+            {
+              icon 
+              ? 
+              <View style={styles.iconViewWrapper}>
+                  <Image style={styles.iconSize} source={icon}/>
+              </View>
+              : null
+            }
+            <Text style={styles.modalMessage}>{messageContent}</Text>
           </View>
-        
         </TouchableWithoutFeedback>
       </TouchableOpacity>
     </Modal>
@@ -23,6 +37,7 @@ AlertPopUp.propTypes = {
   messageContent: PropTypes.string.isRequired,
   callback: PropTypes.func.isRequired,
   visibility: PropTypes.bool.isRequired,
+  icon: PropTypes.node,
 };
 
 export default AlertPopUp;
