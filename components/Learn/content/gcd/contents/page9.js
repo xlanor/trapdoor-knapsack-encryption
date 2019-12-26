476fa07c2e14ce6dee40bcd51b9ccbedec30e6f9
genes3e7 <56685188+genes3e7@users.noreply.github.com>
2019-12-26 19:14:30 +0800

    fixed gcd section fontstyling for title

--- components/Learn/content/gcd/contents/page9.js
+++ components/Learn/content/gcd/contents/page9.js
@@ -1,5 +1,11 @@
 export default pageNine = {
+<<<<<<< HEAD
     "title":"<h1>Extended Euclidean Algorithm</h1>",
     "text": `<p>The point of this algorithm is to find out whether there is a multiplicative inverse and what value it is<br/>
     It is an extension of the standard euclidean algorithm</p>`
+=======
+    "title":"Extended Euclidean Algorithm",
+    "text": `<p>The point of this algorithm is to find out whether there is a multiplicative inverse and what value it is.<br/><br/>
+It is an extension of the standard euclidean algorithm.</p>`
+>>>>>>> 899b01d... fixed gcd section fontstyling for title
 }
---
+++ components/Learn/content/gcd/styles/PicStyle.js
@@ -0,0 +1,25 @@
+import { Dimensions } from 'react-native';
+
+const picStyle = {
+    flex: 1,
+    width: null,
+    height: null,
+    alignItems: 'stretch',
+    resizeMode: 'stretch',
+}
+const containerStyle = {
+    height: Dimensions.get('window').height * 0.75,
+    //alignItems: 'stretch',
+    backgroundColor: '#fff'
+}
+const titleStyle = {
+    fontSize: 30,
+    fontFamily: 'comfortaa-bold',
+    textAlign: 'center',
+}
+
+export default {
+    picStyle,
+    containerStyle,
+    titleStyle
+};
