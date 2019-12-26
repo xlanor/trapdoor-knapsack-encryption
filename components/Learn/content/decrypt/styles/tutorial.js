d0800aa79284d6c2ba050f66377c4ebb7a54190f
genes3e7 <56685188+genes3e7@users.noreply.github.com>
2019-12-26 23:13:11 +0800

    lock down heights
    should theoractically prevent any elements bumping.
    all get their dimensions as a fraction of window height

--- components/Learn/content/decrypt/styles/tutorial.js
+++ components/Learn/content/decrypt/styles/tutorial.js
@@ -56,7 +56,7 @@ const textStyleCiphertext = {
   paddingBottom: Dimensions.get('screen').height * 0.01,
 }
 const learnTabPad = {
-  height: Dimensions.get('window').height * 0.8,
+  height: Dimensions.get('window').height * 0.79,
   marginLeft: Dimensions.get('window').height * 0.05,
   marginRight: Dimensions.get('window').height * 0.05,
   //backgroundColor: '#888'
