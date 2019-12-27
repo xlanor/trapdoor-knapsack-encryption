6c9c6f4e6386efffb78361ab9085635c193117bd
xlanor <contact@jingk.ai>
2019-12-27 12:01:55 +0800

    Merging dev-eugene

--- components/Learn/content/gcd/PicStyle.jsx
+++ components/Learn/content/gcd/PicStyle.jsx
@@ -9,10 +9,7 @@ import {
 } from 'react-native';
 
 import HTML from 'react-native-render-html'
-<<<<<<< HEAD
-=======
 import PropTypes from 'prop-types'
->>>>>>> 2c6a6bb... test gcd content image type
 
 import styles from './styles'
 
@@ -24,11 +21,7 @@ class PicStyle extends Component {
         const { title, img } = this.props;
         return (
             <View style={styles.PicStyle.containerStyle} >
-<<<<<<< HEAD
-                <Text style={styles.PicStyle.titleStyle}>{title}</Text>
-=======
                 <HTML html={title} style={styles.PicStyle.titleStyle} />
->>>>>>> 2c6a6bb... test gcd content image type
                 <Image source={img} style={styles.PicStyle.picStyle} />
             </View>
         )
