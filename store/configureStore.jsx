87d4aca6156f0f34b09c61345667142726de83ff
xlanor <contact@jingk.ai>
2019-12-27 17:40:22 +0800

    Adding redux eprsist

--- store/configureStore.jsx
+++ store/configureStore.jsx
@@ -1,11 +1,19 @@
 import { createStore, combineReducers } from 'redux';
-
+import { persistStore, persistReducer } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 import countReducer from '../reducers/countReducer';
 import lessonPageReducer from '../reducers/lessonPageReducer';
 import currentTabAndPageReducer from '../reducers/currentTabAndPageReducer';
 import currentParametersReducer from '../reducers/currentParametersReducer';
 import currentEncryptionReducer from '../reducers/currentEncryptionReducer';
 import simulatorReducer from '../reducers/simulatorReducer';
+
+
+const persistConfig = {
+  key:"root",
+  storage: AsyncStorage,
+  blacklist: ['simulator'],
+}
 
 const rootReducer = combineReducers(
   { 
@@ -21,5 +29,24 @@ const rootReducer = combineReducers(
 const configureStore = () => {
   return createStore(rootReducer);
 }
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore();
+const persistor = persistStore(store);
 
-export default configureStore;
+
+const getPersistor = () => persistor;
+const getStore = () => store;
+const getState = () => {
+  return store.getState();
+}
+
+export {
+  getStore,
+  getState,
+  getPersistor
+};
+export default {
+  getStore,
+  getState,
+  getPersistor
+}
