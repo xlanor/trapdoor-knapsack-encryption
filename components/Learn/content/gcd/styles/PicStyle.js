9ebb69d7d9aaf50166ea10b9bdf1d5e534f13aca
xlanor <contact@jingk.ai>
2019-12-27 12:01:55 +0800

    Merging dev-eugene

--- components/Learn/content/gcd/styles/PicStyle.js
+++ components/Learn/content/gcd/styles/PicStyle.js
@@ -7,17 +7,6 @@ const picStyle = {
     alignItems: 'stretch',
     resizeMode: 'stretch',
 }
-<<<<<<< HEAD
-const containerStyle = {
-    height: Dimensions.get('window').height * 0.75,
-    //alignItems: 'stretch',
-    backgroundColor: '#fff'
-}
-const titleStyle = {
-    fontSize: 30,
-    fontFamily: 'comfortaa-bold',
-    textAlign: 'center',
-=======
 const containerStyle = { 
     height: Dimensions.get('window').height * 0.8,
     alignItems: 'stretch',
@@ -25,7 +14,6 @@ const containerStyle = {
 }
 const titleStyle ={
     justifyContent: 'center'
->>>>>>> 2c6a6bb... test gcd content image type
 }
 
 export default {
