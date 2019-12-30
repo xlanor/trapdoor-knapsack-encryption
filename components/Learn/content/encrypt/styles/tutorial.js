e9d819a9bb0b789dc778fcfa97336b3cd38abbf3
genes3e7 <56685188+genes3e7@users.noreply.github.com>
2019-12-31 03:53:34 +0800

    remove forcing arrow down for key, encrypt and decrypt

--- components/Learn/content/encrypt/styles/tutorial.js
+++ components/Learn/content/encrypt/styles/tutorial.js
@@ -61,7 +61,7 @@ const textStyleTitleCenter = {
 }
 
 const learnTabPad = {
-  height: Dimensions.get('window').height * 0.79,
+  //height: Dimensions.get('window').height * 0.79,
   marginLeft: Dimensions.get('window').height * 0.05,
   marginRight: Dimensions.get('window').height * 0.05,
   //backgroundColor: '#888'
