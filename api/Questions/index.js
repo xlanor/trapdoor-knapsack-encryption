f3eab9f5bd94c29122f1e6605c3414029e58be94
xlanor <contact@jingk.ai>
2020-01-16 01:07:04 +0800

    Wrote basic axios connection, time to figure out backgrounding

---
+++ api/Questions/index.js
@@ -0,0 +1,27 @@
+import { axios } from '../axios';
+import Header from '../header';
+import common from '../common';
+import mapIndex from '../pageIndexMapper';
+
+const GET_QUESTION_URL='questions/';
+
+export const getQuestions = async(currentTab, succ, fail) => {
+    const apiHeader = Header()
+    .contentType('application/json')
+    .build();
+    
+    let urlIndex = mapIndex(currentTab);
+    try{
+        const res = await axios({
+            headers: apiHeader,
+            method: 'get',
+            url: `${GET_QUESTION_URL}${urlIndex}/`
+        });
+        succ(res.data);
+    }catch(err){
+        fail({
+            other: err.message,
+        });
+    }
+
+}
---
+++ api/header.js
@@ -0,0 +1,20 @@
+import common from './common';
+
+const Header = () => ({
+    res : {},
+
+    contentType(val) {
+        this.res = { ...this.res, 'Content-Type': val || '' }
+        return this;
+    },
+
+
+    token(val) {
+        this.res = { ...this.res, 'x-token': val || '' };
+        return this;
+    },
+    build() {
+      return this.res;
+    },
+  });
+export default Header;
