8cb74338fbb5150de751fc30142b73f4fe4f2c99
xlanor <tanjingkai@me.com>
2020-01-31 01:10:14 +0800

    Added credits base

---
+++ components/Credits/CreditsParent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Text } from 'react-native';
+
+class CreditsParent extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <Text>Hello, World!</Text>
+        )
+    }
+}
+export default CreditsParent;
---
+++ screens/CreditsScreen.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+import { SafeAreaView } from 'react-navigation'
+
+import styles from './styles';
+import CreditsParent from '../components/Credits/CreditsParent'
+
+export default function CreditsScreen() {
+  return (
+    <SafeAreaView style={styles.homeScreen.backGroundContainer}>
+      <View
+        style={styles.homeScreen.backGroundContainer}>
+          <CreditsParent/>
+          
+      </View>
+
+    </SafeAreaView>
+  );
+}
+
+CreditsScreen.navigationOptions = {
+  header: null,
+};
