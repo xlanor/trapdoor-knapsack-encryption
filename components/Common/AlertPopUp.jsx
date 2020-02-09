fed9be6b20b243a9352356fa89d66f9750a16d4d
xlanor <contact@jingk.ai>
2020-02-09 12:56:42 +0800

    Linted all Components

--- components/Common/AlertPopUp.jsx
+++ components/Common/AlertPopUp.jsx
@@ -1,41 +1,27 @@
-import React from 'react'
-import {
-   Modal, 
-   TouchableOpacity, 
-   TouchableWithoutFeedback, 
-   View, 
-   Text, 
-   Image
-} from 'react-native'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Modal, TouchableOpacity, TouchableWithoutFeedback, View, Text, Image } from 'react-native';
+import PropTypes from 'prop-types';
 import { alertPopUp as styles } from './styles';
 
-const AlertPopUp = (props) => {
+const AlertPopUp = props => {
   const { callback, messageContent, visibility, icon, renderedBlocks } = props;
   return (
     <Modal animationType="fade" transparent visible={visibility}>
-      <TouchableOpacity style={styles.modalBackground}  onPress={() => callback()}>
+      <TouchableOpacity style={styles.modalBackground} onPress={() => callback()}>
         <TouchableWithoutFeedback>
           <View style={styles.modalContainer}>
-            {
-              icon 
-              ? 
+            {icon ? (
               <View style={styles.iconViewWrapper}>
-                  <Image style={styles.iconSize} source={icon}/>
+                <Image style={styles.iconSize} source={icon} />
               </View>
-              : null
-            }
-            {
-              renderedBlocks
-              ? renderedBlocks
-              : <Text style={styles.modalMessage}>{messageContent}</Text>
-            } 
+            ) : null}
+            {renderedBlocks || <Text style={styles.modalMessage}>{messageContent}</Text>}
           </View>
         </TouchableWithoutFeedback>
       </TouchableOpacity>
     </Modal>
   );
-}
+};
 
 AlertPopUp.propTypes = {
   messageContent: PropTypes.string,
