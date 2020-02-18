5468be8064ba537770c31f702d2a257ee49cba5a
xlanor <contact@jingk.ai>
2020-02-18 15:45:27 +0800

    Fuck this

--- __tests__/components/Learn/content/KeyGeneration-test.jsx
+++ __tests__/components/Learn/content/KeyGeneration-test.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 // Testing Modules
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
@@ -9,6 +11,60 @@ import thunk from 'redux-thunk';
 
 import { getStore } from '../../../../redux-modules/store/configureStore';
 
+// redux actions
+
+import { ALLOW_NEXT_PAGE_ACTION, NEXT_KEY_PAGE_ACTION } from '../../../../redux-modules/actions/tabPage';
+
+import {
+  UPDATE_PRIVATE_KEY_STRING_ACTION,
+  UPDATE_PRIVATE_KEY_SUM_ACTION,
+  UPDATE_PRIVATE_KEY_ARRAY_ACTION,
+  UPDATE_MODULO_ACTION,
+  UPDATE_MULTIPLIER_ACTION,
+  UPDATE_INVERSE_ACTION,
+  UPDATE_PUBLIC_KEY_STRING_ACTION,
+  UPDATE_PUBLIC_KEY_ARRAY_ACTION,
+} from '../../../../redux-modules/actions/updateParameters';
+
+import { UNLOCK_TROPHY_KEYRING, SHOW_TROPHY_ACTION } from '../../../../redux-modules/actions/manageTrophies';
+
+import {
+  ENCRYPT_LOCK_ACTION,
+  DECRYPT_LOCK_ACTION,
+  ENCRYPT_UNLOCK_ACTION,
+} from '../../../../redux-modules/actions/learnPageLock';
+
+import KeyPage from '../../../../components/Learn/content/key/Tutorial';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+// the mocked start state of our redux.
+const startState = {
+  trophy: getStore().getState().trophy,
+  updateParameters: getStore().getState().updateParameters,
+  lessonPageTabAndPages: getStore().getState().lessonPageTabAndPages,
+};
+
+describe('Components/Learn/KeyGen testing', () => {
+  jest.useFakeTimers();
+  jest.mock('redux-persist/integration/react', () => ({
+    PersistGate: props => props.children,
+  }));
+  it(`Ensure that the switch case renders all pages properly`, async () => {
+    const store = mockStore(startState);
+    const instanceOf = await renderer
+      .create(
+        <Provider store={store}>
+          <KeyPage />
+        </Provider>,
+      );
+      console.log(instanceOf.toJSON());
+      console.log(instanceOf.getInstance());
+    instanceOf.setPrivateKey('yoona');
+    expect(instanceOf.state.currentPrivateKey).toEqual(`yoona`);
+  });
+});
 /*
 Preparing for tomorrow
 
